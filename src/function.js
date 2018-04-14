13a1edc0e9e68be6bbcf610c192a8c45cd19f0e2
unknown <petrus.dsw@gmail.com>
2018-04-14 19:22:29 +0700

    Fix client functionality

--- src/function.js
+++ src/function.js
@@ -5,6 +5,20 @@ $( document ).ready(function() {
 		var keyword = $("input[name='input-keyword-search']").val();
 		var count = $("input[name='input-tweet-count']").val();
 		
+		// $.ajax({
+					// type: "POST",
+					// url: "http://127.0.0.1:5000/fetch",
+					// data: {
+						// 'keyword' : keyword
+					// },
+					// success: function(data) {
+						// data.forEach( function(record) {
+							// console.log(record);
+						// } )
+					// }
+				// });
+		
+		
 		$.ajax({
 			
 			type: "POST",
@@ -23,44 +37,35 @@ $( document ).ready(function() {
 					success: function(data) {
 						data.forEach( function(record) {
 							
+							var predictedImageCategory = record.predicted_image_string ? record.predicted_image_string : "-";
+							$("#h1-predicted-image-category").text(predictedImageCategory);
+							
+							$("#span-image-category").text(predictedImageCategory);
+							
+							var predictedImageProbability = record.probability_image ? record.probability_image : "-";
+							$("#h2-predicted-image-probability").text(predictedImageProbability);
+							
+							
+							var imageUrl = record.image_url ? record.image_url : "http://d2pa5gi5n2e1an.cloudfront.net/id/images/common/no_image_l.gif";
+							$("#anchor-image").attr("href", imageUrl);
+							$("#img-image").attr("src", imageUrl);
+							
+							$("#p-processed-tweet").text(record.processed_tweet);
+							$("#p-em-original-tweet").text(record.original_tweet);
+							
+							$("#h1-predicted-text-category").text(record.predicted_text_string);
+							
+							$("#span-text-category").text(record.predicted_text_string);
+							
+							$("#h2-predicted-text-probability").text(record.probability_text);
+							
+							$("#div-single-content").clone().attr("style", "").appendTo("#div-collection-content");
+							
 						} )
 					}
 				})
 			}
 			
-		});
-		
-		$.ajax({
-			type: "POST",
-			url: "http://127.0.0.1:5000/fetch",
-			data: {
-				'keyword' : keyword
-			},
-			success: function(data) {
-				console.log(data);
-				data.forEach( function(el) {
-					
-					$("#span-content-id").text(el.id);
-					
-					var div = document.getElementById('div-predicted-image-category');
-					div.innerHTML = 'Predicted Image Category: ' + (el.predicted_image_category ? el.predicted_image_category : '-');
-					
-					var div = document.getElementById('probability-predicted-image');
-					div.innerHTML = 'Predicted Image Probability: ' + (el.probability_image ? el.probability_image : '-');
-					
-					var div = document.getElementById('div-predicted-text-category');
-					div.innerHTML = 'Predicted Text Category: ' + (el.predicted_text_string ? el.predicted_text_string : '-');
-					
-					var div = document.getElementById('probability-predicted-text');
-					div.innerHTML = 'Predicted Text Probability: ' + (el.probability_text ? el.probability_text : '-');
-					
-					$("#h2-processed-tweet").text(el.processed_tweet);
-					
-					$("#h2-original-tweet").text(el.original_tweet);
-					
-					$("#div-single-content").clone().attr('style', '').appendTo($("#div-collection-content"));
-				});
-			}
 		});
 		
 		
